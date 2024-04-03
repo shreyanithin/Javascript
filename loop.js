cd8cc1ce3a10d014b9ad23149b978d9af8570610
shreyanithin <shreyanithinr@gmail.com>
2024-04-03 22:23:56 +0530

    alternate method

--- loop.js
+++ loop.js
@@ -1,9 +1,20 @@
-for(let i=1;i<=5;i++){
-    for (let j = 1;  j <= i; j++) {
+// for(let i=1;i<=5;i++){
+//     for (let j = 1;  j <= i; j++) {
         
-        process.stdout.write('*');  
-    }
-    console.log(
-        
-    );
+//         process.stdout.write('*');  
+//     }
+//     console.log(
+
+//     );
+// }
+
+//alternate method
+
+let s = '';
+for(let i = 1; i <= 5; i++){
+  for(let j = 1; j <= i; j++){
+    s+= '*';
+  }
+  s += "\n";
 }
+console.log(s);
